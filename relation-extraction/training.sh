4b2dd531b66e475b576e7ceccd8827c2380e6fad
Can <canbeslendi@live.de>
2022-05-04 12:25:55 +0200

    Versuche die Relation Extraction zu trainieren

---
+++ relation-extraction/training.sh
@@ -0,0 +1,27 @@
+export SAVE_DIR=./output
+export DATA="GAD"
+export SPLIT="1"
+export DATA_DIR=../datasets/RE/${DATA}/${SPLIT}
+export ENTITY=${DATA}-${SPLIT}
+
+export MAX_LENGTH=128
+export BATCH_SIZE=32
+export NUM_EPOCHS=3
+export SAVE_STEPS=1000
+export SEED=1
+
+python run_re.py \
+    --task_name SST-2 \
+    --config_name bert-base-cased \
+    --data_dir ${DATA_DIR} \
+    --model_name_or_path dmis-lab/biobert-base-cased-v1.1 \
+    --max_seq_length ${MAX_LENGTH} \
+    --num_train_epochs ${NUM_EPOCHS} \
+    --per_device_train_batch_size ${BATCH_SIZE} \
+    --save_steps ${SAVE_STEPS} \
+    --seed ${SEED} \
+    --do_train \
+    --do_predict \
+    --learning_rate 5e-5 \
+    --output_dir ${SAVE_DIR}/${ENTITY} \
+    --overwrite_output_dir
